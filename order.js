a0ad295156a5c0c204d6605494c9e504a7ba27aa
flavioespinoza <flavio.espinoza@gmail.com>
2018-06-20 20:01:03 -0600

    Finished async order functions.

--- order.js
+++ order.js
@@ -1,46 +1,107 @@
 const log = require('ololog').configure({locate: false})
 const chance = require('chance').Chance()
+const Client = require('node-rest-client').Client
+const client = new Client()
+
+// const url = 'https://api.gdax.com/products/btc-usd/book'
+const url = 'https://min-api.cryptocompare.com/data/histominute?fsym=EMC2&tsym=BTC&limit=20&aggregate=1&e=bittrex'
 
 /** First In, First Out */
 function __timestamp () {
   return (new Date).getTime()
 }
 
-function __utc () {
-  return new Date().toJSON().toString()
-}
+const Order = async function (type, side, order) {
 
-const Order = (type, side, order) => {
+  let timestamp = __timestamp()
 
-  const state = {
-    id: chance.guid({version: 5}),
-    type,
-    side,
+  return new Promise(function (resolve) {
+
+    client.get(url, async function (data) {
+
+      if (order.params.oid) {
+        order.params.oid = order.params.oid + '_' + timestamp
+      }
+
+      const state = {
+        id: chance.guid({version: 5}),
+        type,
+        side,
+        timestamp,
+        data: data.Data
+      }
+
+      resolve(await Object.assign({}, state, order))
+
+    })
+
+  })
+
+};
+
+const BuyLimit = async function (price, amount, params) {
+
+  try {
+
+    let order = {
+      price,
+      amount,
+      params
+    }
+
+    return await Order('buy', 'limit', order)
+      .then(async function (order_res) {
+
+        return order_res
+
+      })
+      .catch(function (err) {
+        log.lightYellow('CATCH:', err)
+      })
+
+  } catch (__err) {
+
+    log.lightYellow(__err)
+
   }
 
-  return Object.assign({}, state, order)
+};
 
-}
+const SellLimit = async function (price, amount, params) {
 
-const BuyLimit = function (price, amount, params) {
-  let order = {
-    price,
-    amount,
-    params,
+  try {
+
+    let order = {
+      price,
+      amount,
+      params
+    }
+
+    return await Order('sell', 'limit', order)
+      .then(async function (order_res) {
+
+        return order_res
+
+      })
+      .catch(function (err) {
+        log.lightYellow('CATCH:', err)
+      })
+
+  } catch (__err) {
+
+    log.lightYellow(__err)
+
   }
-  return Order('buy', 'limit', order)
-}
 
-let buy_order = BuyLimit(100, 30, {oid: 'Stimpy', timestamp: __timestamp()})
-
-log.lightBlue(JSON.stringify(buy_order, null, 2))
+};
 
 
-const SellLimit = function (price, amount, params) {
-  let order = {
-    price,
-    amount,
-    params,
-  }
-  return Order('buy', 'limit', order)
-}
+(async function () {
+
+  let buy_order = await BuyLimit(100, 30, {oid: 'stimpy_' + chance.hash({length: 8})})
+  log.lightBlue(JSON.stringify(buy_order, null, 2))
+
+  let sell_order = await SellLimit(100, 30, {oid: 'ren_' + chance.hash({length: 8})})
+  log.lightRed(JSON.stringify(sell_order, null, 2))
+
+})()
